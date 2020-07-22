5707bc3905ad477f97918e05da8afed83fa7d3b0
86gumball <lucasruben2000@gmail.com>
2020-07-22 16:48:19 +0000

    Add functional user feed
    User feed now correctly displays all posts of a given user
    
    Both normal feed and user feed (myfeed) now show the poster's
    username instead of the user's ID
    
    Miscellaneous:
      - User now has a .username() method
      - Added a debug print showing current user logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def myfeed
     @userposts = User.find(params[:id]).posts
+    @userid = User.find(params[:id]).id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts
+
+  def username
+    self.email.split('@')[0]
+  end
 end
